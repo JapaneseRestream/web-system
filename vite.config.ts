3fc4c7041e1c275029fa7e29509efc9fbd729a69
Keiichiro Amemiya <hoishinxii@gmail.com>
2024-08-21 19:43:51 +0900

    eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,11 @@
-import {
-  vitePlugin as remix,
-  cloudflareDevProxyVitePlugin as remixCloudflareDevProxy,
-} from "@remix-run/dev";
+import remix from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
-    remixCloudflareDevProxy(),
-    remix({
+    remix.cloudflareDevProxyVitePlugin(),
+    remix.vitePlugin({
       future: {
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
