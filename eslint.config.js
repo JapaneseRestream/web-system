94c0fc6d5b27f216513ed9c418a5f1633c054daa
Keiichiro Amemiya <hoishinxii@gmail.com>
2024-08-21 19:54:19 +0900

    add prettier

--- eslint.config.js
+++ eslint.config.js
@@ -5,34 +5,34 @@ import simpleImportSort from "eslint-plugin-simple-import-sort";
 import typescriptEslint from "typescript-eslint";
 
 export default typescriptEslint.config(
-  eslint.configs.recommended,
-  ...typescriptEslint.configs.strictTypeChecked,
-  ...typescriptEslint.configs.stylisticTypeChecked,
-  {
-    languageOptions: {
-      parserOptions: {
-        projectService: {
-          allowDefaultProject: ["eslint.config.js"],
-          defaultProject: "./tsconfig.json",
-        },
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
-  },
-  {
-    rules: {
-      "@typescript-eslint/no-unsafe-assignment": "off",
-      "@typescript-eslint/no-unused-vars": "off",
-    },
-  },
+	eslint.configs.recommended,
+	...typescriptEslint.configs.strictTypeChecked,
+	...typescriptEslint.configs.stylisticTypeChecked,
+	{
+		languageOptions: {
+			parserOptions: {
+				projectService: {
+					allowDefaultProject: ["eslint.config.js"],
+					defaultProject: "./tsconfig.json",
+				},
+				tsconfigRootDir: import.meta.dirname,
+			},
+		},
+	},
+	{
+		rules: {
+			"@typescript-eslint/no-unsafe-assignment": "off",
+			"@typescript-eslint/no-unused-vars": "off",
+		},
+	},
 
-  {
-    plugins: {
-      "simple-import-sort": simpleImportSort,
-    },
-    rules: {
-      "simple-import-sort/imports": "error",
-      "simple-import-sort/exports": "error",
-    },
-  }
+	{
+		plugins: {
+			"simple-import-sort": simpleImportSort,
+		},
+		rules: {
+			"simple-import-sort/imports": "error",
+			"simple-import-sort/exports": "error",
+		},
+	},
 );
